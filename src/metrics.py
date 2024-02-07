d8fa2f6a97cd995645e749684d64eaec5a46e839
Amit Bhanja <amit.bhanja@sixsq.com>
2024-02-07 11:16:51 +0100

    Added prometheus client
    -- Defined metrics

--- src/metrics.py
+++ src/metrics.py
@@ -1,6 +1,8 @@
 from prometheus_client import Counter
 
-NOTIFICATIONS_SENT = Counter('notifications_sent', 'Number of notifications sent',
+NAMESPACE = 'kafka_notify'
+
+NOTIFICATIONS_SENT = Counter(f'{NAMESPACE}_notifications_sent', 'Number of notifications sent',
                              ['type', 'subscription_name', 'endpoint'])
-NOTIFICATIONS_ERROR = Counter('notifications_error', 'Number of notifications that could not be sent due to error',
+NOTIFICATIONS_ERROR = Counter(f'{NAMESPACE}_notifications_error', 'Number of notifications that could not be sent due to error',
                               ['type', 'subscription_id', 'endpoint', 'exception'])
