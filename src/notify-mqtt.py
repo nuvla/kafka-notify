059ffe80937dc38caa9e5dcbe48e40c1b2d9fcd2
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2024-06-17 23:18:16 +0200

    update building of outgoing message and fix deserialisation

--- src/notify-mqtt.py
+++ src/notify-mqtt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import json
 import multiprocessing
 import os
 import threading
@@ -20,10 +20,6 @@ log_local = get_logger('mqtt-notifier')
 
 def message_content(msg_params: dict) -> dict:
     log_local.debug('Building message content for: %s', msg_params)
-
-    r_uri = msg_params.get('RESOURCE_URI')
-    link_text = msg_params.get('RESOURCE_NAME') or r_uri
-    component_link = f'<{NUVLA_ENDPOINT}/ui/{r_uri}|{link_text}>'
 
     for key, value in msg_params.items():
         if value is None:
@@ -48,18 +44,11 @@ def message_content(msg_params: dict) -> dict:
 
     result = {}
     for attr in attrs:
-        if msg_params.get(attr):
-            result[attr] = msg_params.get(attr)
+        if attr in msg_params:
+            result[attr.lower()] = msg_params.get(attr)
 
-    # Order of the fields defines the layout of the message
-    if msg_params.get('TRIGGER_RESOURCE_PATH'):
-        resource_path = msg_params.get('TRIGGER_RESOURCE_PATH')
-        resource_name = msg_params.get('TRIGGER_RESOURCE_NAME')
-        trigger_link = \
-            f'<{NUVLA_ENDPOINT}/ui/{resource_path}|{resource_name}>'
-        result['TRIGGER_LINK'] = trigger_link
-
-    result['COMPONENT_LINK'] = component_link
+    result['component_link'] = \
+        f'{NUVLA_ENDPOINT}/{msg_params.get("RESOURCE_URI")}'
 
     return result
 
@@ -75,13 +64,13 @@ def extract_destination(dest: str) -> tuple:
     return host, port, uri
 
 
-def send_mqtt_notification(payload, mqtt_server: str):
+def send_mqtt_notification(payload: str, mqtt_server: str):
     host, port, topic = extract_destination(mqtt_server)
     log_local.info(f'Sending message to {host}:{port}/{topic}')
     return mqtt_publish.single(topic, payload, hostname=host, port=int(port))
 
 
-def send_message(payload, mqtt_server):
+def send_message(payload: str, mqtt_server):
     def thread_function():
         try:
             send_mqtt_notification(payload, mqtt_server)
@@ -106,7 +95,8 @@ def worker(workq: multiprocessing.Queue):
         subs_name = msg.value.get('NAME') or msg.value['SUBS_NAME']
         dest = msg.value['DESTINATION']
         try:
-            send_message(message_content(msg.value), dest)
+            msg_dict = message_content(msg.value)
+            send_message(json.dumps(msg_dict), dest)
         except Exception as ex:
             err_msg = f'Failed sending message: {subs_name} to {dest}'
             log_local.error(err_msg)
